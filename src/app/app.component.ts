f9e995779d8430ac74b6c5370bb937426b54b034
Lejla1502 <lejlasphotmail.com>
2022-02-01 15:45:07 +0100

    Adding loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts:Post[] = [];
+  isFetching=false;
 
   constructor(private http: HttpClient) {}
 
@@ -37,6 +38,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts(){
+    this.isFetching=true;
     this.http.get<{[key:string]:Post}>('https://ng-complete-guide-e6189-default-rtdb.firebaseio.com/posts.json')
     .pipe(map(responseData=>{
       const postsArray:Post[]=[];
@@ -50,6 +52,7 @@ export class AppComponent implements OnInit {
       return postsArray;
     }))
     .subscribe(posts=>{
+      this.isFetching=false;
       this.loadedPosts=posts;
     });
   }
