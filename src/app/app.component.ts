f4b4d5cf3803b0f9aa50e9d92378eea4eb404534
Lejla1502 <lejlasphotmail.com>
2022-02-01 13:20:34 +0100

    Using HTTP POST and GET requests

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    this.http
+    .post('https://ng-complete-guide-e6189-default-rtdb.firebaseio.com/posts.json', postData)
+    .subscribe(responseData=>{
+      console.log(responseData);
+    });
+    console.log(postData);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts(){
+    this.http.get('https://ng-complete-guide-e6189-default-rtdb.firebaseio.com/posts.json')
+    .subscribe(posts=>{
+      console.log(posts);
+    });
+  }
+}
