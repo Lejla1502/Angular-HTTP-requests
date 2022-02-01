98fd7fe77b9011c7e1a498f435ae95136e6bc581
Lejla1502 <lejlasphotmail.com>
2022-02-01 14:16:26 +0100

    Using types with an HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -19,7 +20,7 @@ export class AppComponent implements OnInit {
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     this.http
-    .post('https://ng-complete-guide-e6189-default-rtdb.firebaseio.com/posts.json', postData)
+    .post<{name:string}>('https://ng-complete-guide-e6189-default-rtdb.firebaseio.com/posts.json', postData)
     .subscribe(responseData=>{
       console.log(responseData);
     });
@@ -36,9 +37,9 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts(){
-    this.http.get('https://ng-complete-guide-e6189-default-rtdb.firebaseio.com/posts.json')
+    this.http.get<{[key:string]:Post}>('https://ng-complete-guide-e6189-default-rtdb.firebaseio.com/posts.json')
     .pipe(map(responseData=>{
-      const postsArray=[];
+      const postsArray:Post[]=[];
       for(const key in responseData){
         if(responseData.hasOwnProperty(key))
         {
---
+++ src/app/post.model.ts
@@ -0,0 +1,5 @@
+export interface Post{
+    title:string,
+    content:string,
+    id?:string //this meands that the id is optional
+}
