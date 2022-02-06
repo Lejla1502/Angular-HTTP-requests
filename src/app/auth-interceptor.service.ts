57c5e759e8bcc99537dae89c5bbdd6efc44464bb
Lejla1502 <lejlasp@hotmail.com>
2022-02-06 11:36:42 +0100

    Modifying request object

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -4,7 +4,14 @@ import { Observable } from "rxjs";
 export class AuthInterceptorService implements HttpInterceptor{
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         console.log('request is on its way'); //the code that is run right before the request leaves the app
-       return next.handle(req); //we let the request to continue and 
+
+        const modifiedReq=req.clone({
+            headers:req.headers.append('Auth','xyz')  //here we are creating a new request and appending headers from the old one
+                                                    //plus adding a new header-which doesn't do anything
+                                                    //but will be created for each and every request
+        });
+
+       return next.handle(modifiedReq); //we let the request to continue and 
        //we should actually return the result to really let it continue
        //if we don't return it and pass the request then the reuest will not continue and therefore the app will break
     }
